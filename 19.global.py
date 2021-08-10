5dcc3a2eb769e1b8727a93851f2dfb21283a9856
Tanmoy Sarkar <s.tanmoy613@gmail.com>
2021-08-10 21:11:06 +0530

    global file commit

---
+++ 19.global.py
@@ -0,0 +1,8 @@
+x = 50
+
+def add():
+    x = 20
+    print("value of x ",x)
+
+print("Tanmoy")    
+    
---
+++ 4.list_item_using_while.py
@@ -0,0 +1,6 @@
+print("Let's iterate list items using while.")
+list = [8,6,9,5,15,89]
+i=0
+while i <= len(list):
+    print(list[i])
+    i=i+1
